3e853ebd462f4e14618d5c975f93131a0020bbc8
son9912 <son9912@gmail.com>
2017-03-09 19:51:21 +0700

    I fux up, so I really need help
    Help please.

--- main.c
+++ main.c
@@ -1,4 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+void tutorials();
+void switchhub();
+
 int main(){
-  printf("I have a pen.\nI have an apple.\nApple pen.\nI have a pen.\nI have pineapple.\nPineapple Pen.");
+    void tutorials();
+    return 0;
+}
+
+void tutorials(){
+// This function will give the user instructions to switch to the correct functions.
+    printf("Initializing the POS Systems...\n");
+    printf("[--------------------------------------------------]\n");
+    printf("|          Hello! This is the POS Systems!         |\n");
+    printf("[--------------------------------------------------]\n");
+    printf("               What do you like to do?\n");
+    printf("1: Login \t\t\t\t\t2: Check Inventory\n3: Check inventory\t\t\t4: Quit\n");
+    void switch_hub();
+}
+
+void switch_hub(){
+// This function will be the startup program interface.
+    printf("Please type in your desire! >>");
+    char functionscall;
+    scanf("%[^\n]",&functionscall);
+    switch (functionscall){
+        case ('1'):
+
+            break;
+        case ('2'):
+
+            break;
+        case ('3'):
+
+            break;
+        default:
+            printf("Your input is unregcognizable. Please try again.");
+            void switch_hub();
+    }
 }
