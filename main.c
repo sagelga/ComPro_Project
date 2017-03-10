8c3a5dda5a2196941fa3a0ad88e6ad8d5b89a543
son9912 <son9912@gmail.com>
2017-03-10 09:09:05 +0700

    Functions shifting does not work

--- main.c
+++ main.c
@@ -3,31 +3,34 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
-void tutorials();
-void switchhub();
+void switch_hub(text);
 
 int main(){
-    void tutorials();
+// This function will throw in Interface functions to the users.
+    printf("[--------------------------------------------------]\n|          Hello! This is the POS Systems!         |\n[--------------------------------------------------]\n");
+    printf("Please type '1' to start\n");
+    int text;
+    scanf("%d",&text);
+
+    if (text == 1)
+        switch_hub(text);
     return 0;
 }
 
-void tutorials(){
-// This function will give the user instructions to switch to the correct functions.
-    printf("Initializing the POS Systems...\n");
-    printf("[--------------------------------------------------]\n");
-    printf("|          Hello! This is the POS Systems!         |\n");
-    printf("[--------------------------------------------------]\n");
-    printf("               What do you like to do?\n");
-    printf("1: Login \t\t\t\t\t2: Check Inventory\n3: Check inventory\t\t\t4: Quit\n");
-    void switch_hub();
-}
-
-void switch_hub(){
+void switch_hub(text){
 // This function will be the startup program interface.
+    printf("\t\t\tWhat do you like to do?\n");
+
+    // Choose the following functions. Can be programmed in 1 character (from 1-9 and a-z and A-Z)
+    printf("1: Login \t\t\t\t\t2: Check Inventory\n3: Check inventory\t\t\t4: Quit\n");
+
     printf("Please type in your desire! >>");
     char functionscall;
     scanf("%[^\n]",&functionscall);
+
+    // Calls other function that are separately programmed
     switch (functionscall){
         case ('1'):
 
@@ -39,7 +42,9 @@ void switch_hub(){
 
             break;
         default:
+            // Route back to ask for a valid input
             printf("Your input is unregcognizable. Please try again.");
-            void switch_hub();
+            //switch_hub();
     }
+    //return 0;
 }
