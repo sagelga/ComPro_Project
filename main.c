eb7a41aa97c69b723f28266d5bce2e35e6c29338
son9912 <son9912@gmail.com>
2017-03-11 21:59:39 +0700

    Margin Redo

--- main.c
+++ main.c
@@ -6,24 +6,27 @@
 #include <time.h>
 #include <ctype.h>
 
+// Declare all the functions that will be used.
 void switch_hub();
 void terminate();
+void terminal_ruler();
 
 int main(){
 // This function will throw in Interface functions to the users.
-    printf("[--------------------------------------------------]\n|          Hello! This is the POS Systems!         |\n[--------------------------------------------------]\n");
+    printf("[-----------------------------------------------]\n|\tHello!\tThis is the POS Systems!\t|\n[-----------------------------------------------]\n");
     switch_hub();
     return 0;
 }
 
 void switch_hub() {
-// This function will be the startup program interface.
-    printf("\n\n\t\tWhat do you like to do?\n____________________________________________________\n");
+// This function will be the STARTUP PROGRAM INTERFACES
+    char username[] = "Kumamon";
+    printf("\n\nWhat do you like to do?\t\tSign in as %.6s\n_________________________________________________\n",username);
 
     // Choose the following functions. Can be programmed in 1 character (from 1-9 and a-z and A-Z)
-    printf("1: Login \t\t\t2: Check Inventory\n3: Check inventory\t\t4: Quit\n");
+    printf("1: Login\t\t2: Check Stock\n3: Sales Export\t\t4: Quit\n");
 
-    printf("Please type in your desire! >>");
+    printf("\nPlease type in your desire! >>> ");
     char functionscall;
     scanf(" %c", &functionscall);
 
@@ -42,9 +45,14 @@ void switch_hub() {
             printf("Are you really sure about this? (Type Y or N) >>");
             terminate();
             return;
+        case('S'):
+            // Calling the >>terminal_ruler<< function
+            printf("Ok. We will try to configure your screen");
+            terminal_ruler();
+            return;
         default:
-            // Route back to ask for a valid input
-            printf("Your input is unregcognizable. Please try again.\n");
+            // The input is invalid. Trying to route back to ask for a valid input
+            printf("Your input is invalid. Please try again.\n\n\n");
             switch_hub();
             return;
     }
@@ -54,12 +62,13 @@ void terminate(){
     // Type in the text that will be displayed after the user wants to terminate the program
     char text;
     scanf(" %c",&text);
+    text = toupper(text);
     switch(text){
         case ('Y'):
             printf("Have a good luck. Bye Bye!");
             return;
         case ('N'):
-            printf("I'm so happy that you're back! Redirecting.......");
+            printf("I'm so happy that you're back! Redirecting.......\n\n");
             switch_hub();
             return;
         default:
@@ -67,4 +76,8 @@ void terminate(){
             terminate();
             return;
     }
+}
+
+void terminal_ruler(){
+
 }
