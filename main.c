e79ab7bb358bc97f3f3acd26344c1c8c217c1387
Noppanut Ploywong <noppanut15@hotmail.com>
2017-04-06 02:31:34 +0700

    Add the query functions for Promotion table

--- main.c
+++ main.c
@@ -982,6 +982,76 @@ int customerDelete(char *id){
     return 0;   // Not found the given `id` in the records
 }
 
+int promotionSelectById(char *id, double *price, int *status){
+    int numberOfRecords;    // Number of the records in a table
+    numberOfRecords = RecordCount.promotion;
+    for(int i = 0; i < numberOfRecords; i++){
+        if(strcmp(Promotion[i].id, id) == 0){
+            // Return all values back by reference
+            *price = Promotion[i].price;
+            *status = Promotion[i].status;
+
+            return 1;   // Found a record
+        }
+    }
+    return 0;           // Not found the given `id` in the records
+}
+
+void promotionInsert(char *id, double price, int status){
+    int tailIndex = RecordCount.promotion;
+
+    strcpy(Promotion[tailIndex].id, id);
+    Promotion[tailIndex].price = price;
+    Promotion[tailIndex].status = status;
+
+    RecordCount.promotion++;    // Update the amount of records
+
+    promotionFileWrite();       // Save to a Database file
+}
+
+int promotionUpdatePrice(char *id, double price){
+    int numberOfRecords;    // Number of the records in a table
+    numberOfRecords = RecordCount.promotion;
+    for(int i = 0; i < numberOfRecords; i++){
+        if(strcmp(Promotion[i].id, id) == 0){
+            Promotion[i].price = price;
+            promotionFileWrite();    // Save to a Database file
+            return 1;               // Record successfully updated
+        }
+    }
+    return 0;   // Not found the given `id` in the records
+}
+
+int promotionUpdateStatus(char *id, int status){
+    int numberOfRecords;    // Number of the records in a table
+    numberOfRecords = RecordCount.promotion;
+    for(int i = 0; i < numberOfRecords; i++){
+        if(strcmp(Promotion[i].id, id) == 0){
+            Promotion[i].status = status;
+            promotionFileWrite();    // Save to a Database file
+            return 1;               // Record successfully updated
+        }
+    }
+    return 0;   // Not found the given `id` in the records
+}
+
+int promotionDelete(char *id){
+    int numberOfRecords;    // Number of the records in a table
+    numberOfRecords = RecordCount.promotion;
+    for(int i = 0; i < numberOfRecords; i++){
+        if(strcmp(Promotion[i].id, id) == 0){
+            while(i < numberOfRecords - 1){
+                Promotion[i] = Promotion[i+1];
+                i++;
+            }
+            RecordCount.promotion--;    // Update the amount of records
+            promotionFileWrite();       // Save to a Database file
+            return 1;                  // Record successfully deleted
+        }
+    }
+    return 0;   // Not found the given `id` in the records
+}
+
 
 /*
 
